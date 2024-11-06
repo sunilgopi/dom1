b0b931cd0499bdb3d716996863dd7d6d6864ab6d
sunilgopi <118232324+sunilgopi@users.noreply.github.com>
2024-11-06 14:17:00 +0530

    Create js.js

---
+++ js.js
@@ -0,0 +1,17 @@
+function a()
+{
+  let b = document.getElementById("h1")
+  b.textContent="hello world"
+  b.style.fontSize="25px"
+  b.style.backgroundColor="red"
+}
+function  a1()
+{
+  let b1 = document.getElementById("hello")
+  let b2 = document.getElementById("hello1")
+  let b3 = document.getElementById("hello2")
+  
+  b1.innerHTML=`<img style="border-radius: 30%;" src="../manjuproject/assests/images/background.png" alt="" width="200">`
+  b2.innerHTML=`<img style="border-radius: 40%;" src="../manjuproject/assests/images/background.png" alt="" width="200">`
+  b3.innerHTML=`<img style="border-radius: 50%;" src="../manjuproject/assests/images/background.png" alt="" width="200">`
+}
