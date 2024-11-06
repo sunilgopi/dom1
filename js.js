5bd4e4214cce91374de84f7290a26be000a59649
sunilgopi <118232324+sunilgopi@users.noreply.github.com>
2024-11-06 14:22:56 +0530

    Update js.js

--- js.js
+++ js.js
@@ -11,7 +11,7 @@ function  a1()
   let b2 = document.getElementById("hello1")
   let b3 = document.getElementById("hello2")
   
-  b1.innerHTML=`<img style="border-radius: 30%;" src="../manjuproject/assests/images/background.png" alt="" width="200">`
-  b2.innerHTML=`<img style="border-radius: 40%;" src="../manjuproject/assests/images/background.png" alt="" width="200">`
-  b3.innerHTML=`<img style="border-radius: 50%;" src="../manjuproject/assests/images/background.png" alt="" width="200">`
+  b1.innerHTML=`<img style="border-radius: 30%;" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" width="200">`
+  b2.innerHTML=`<img style="border-radius: 40%;" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" width="200">`
+  b3.innerHTML=`<img style="border-radius: 50%;" src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" width="200">`
 }
